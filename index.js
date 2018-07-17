2915cda00cdaa21469da185b5b4968b5e31ecf2f
cjoannes@tulane.edu <cjoannes@tulane.edu>
2018-07-17 14:47:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function add(a, b) { return a + b }
 function subtract(a, b) { return a - b }
 function multiply(a, b) { return a * b }
 function divide(a, b) { return a / b }
-function inc(n) { return n += }
+function inc(n) { return number += n }
 function dec(n) { return a -= b }
 function makeInt(n) { return parseInt('n', 10) }
 function preserveDecimal(n) { return parseFloat('n') }
